f8872460d5edbe09caa33ba7e8843e97336ba743
Baqar Ali <bashah@digital-dividend.com>
2022-11-29 11:44:07 +0500

    develop

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request main!")
-    res.send('Yo Main!')
+    console.log("Just got a request develop!")
+    res.send('Yo Develop!')
 })
 app.listen(process.env.PORT || 3000)
